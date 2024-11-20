02be5efb000d7ef93b46d8ae52677af701967332
DinDevCons <din@devcons.com>
2024-11-20 14:11:40 +0100

    removed unused inports

--- src/wrappers/QueryWrapper.tsx
+++ src/wrappers/QueryWrapper.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { ChildrenProps } from '@/interfaces/children';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
 
 const QueryWrapper = ({ children }: ChildrenProps) => {
   const queryClient = new QueryClient();
